2315d398f143910b70f7234c04eb7aa48f4b1f68
deR0R0 <rorozee1@gmail.com>
2025-09-11 08:00:21 -0400

    fix: make nav slightly smaller

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
 import Navigation from "@/components/Navigation";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Inturn Landing",
+  description: "Inturn is a platform for high school students to find internships at small or startup companies. Helping start ups find the talent they need to grow, and helping student gain experience in the field.",
+}
 
 export default function Home() {
   return (
