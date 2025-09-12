f0e05650b1885c9c72bebed74377095ea8732bfa
deR0R0 <rorozee1@gmail.com>
2025-09-12 08:18:24 -0400

    feat: add hook text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
+import Hook from "@/components/Hook";
 import Navigation from "@/components/Navigation";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Inturn Landing",
   description: "Inturn is a platform for high school students to find internships at small or startup companies. Helping start ups find the talent they need to grow, and helping student gain experience in the field.",
+  keywords: ["internships", "high school", "startups", "small business", "students", "career", "experience", "high school internships", "student curated"],
 }
 
 export default function Home() {
   return (
     <>
       <Navigation />
+      <Hook />
     </>
   );
 }
